7ca96169d8e63d98439fbae04f1516e8de184473
jaikaran109 <jaik05581@gmail.com>
2026-07-02 23:06:37 +0530

    Update sales.mongodb.js

--- Aggregation-Pipelines/sales.mongodb.js
+++ Aggregation-Pipelines/sales.mongodb.js
@@ -9,3 +9,31 @@ db.sales.insertMany([
 { _id: 4, item: "Tomato", price: 6, quantity: 8, category: "Vegetable" },
   { _id: 5, item: "Mango", price: 15, quantity: 3, category: "Fruit" }
 ]);
+
+
+
+// Match - if Match found
+db.sales.aggregate([
+  {$match : {category : "Fruit"}}
+]);
+
+
+
+// Filtered Projection - isme phle match found kr rha h uske baad jo matches h uska vo jo fields chahiye ($project) wala operation perform kr rha h
+db.sales.aggregate([
+  {$match : {category : "Fruit"}},
+  {$project : {_id : 0 , item : 1 , quatity : 1}}
+]);
+
+
+
+// Group - ye group form krta h
+db.sales.aggregate([
+  {
+    $group : {
+      _id : "$cateory",
+      totalSales : {$sum : {$multiply : ["$price" , "$quantity"]}}
+    }
+  }
+])
+
