1ce73e6d0feb9360a1b680f27fe2bf3e5aa5f993
jaikaran109 <jaik05581@gmail.com>
2026-07-02 22:57:22 +0530

    Create sales.mongodb.js

---
+++ Aggregation-Pipelines/sales.mongodb.js
@@ -0,0 +1,11 @@
+// ek stage ka Output dusre stage ka Input hota h 
+
+
+// use DataBaseName_
+db.sales.insertMany([
+{ _id: 1, item: "Apple", price: 10, quantity: 5, category: "Fruit" },
+{ _id: 2, item: "Banana", price: 5, quantity: 10, category: "Fruit" },
+{ _id: 3, item: "Carrot", price: 8, quantity: 6, category: "Vegetable" },
+{ _id: 4, item: "Tomato", price: 6, quantity: 8, category: "Vegetable" },
+  { _id: 5, item: "Mango", price: 15, quantity: 3, category: "Fruit" }
+]);
